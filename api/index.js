d7a525bdea2641bcf734cdb9c5b8140fcb94ab03
dimassdoubles <codedimas@gmail.com>
2024-06-04 06:32:29 +0700

    use vercel handler replace express

--- api/index.js
+++ api/index.js
@@ -4,31 +4,10 @@ require('dotenv').config();
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
-const express = require('express');
-const app = express();
-
 const PORT = process.env.PORT || 3000;
 let SERVER_KEY = process.env.MIDTRANS_SERVER_KEY;
 
-app.post('/pay', async (req, res) => {
-
-  const body = req.body;
-  const orderId = body["order_id"];
-  const grossAmount = body["gross_amount"];
-  let trxSignature = sha512(orderId + grossAmount + SERVER_KEY);
-
-  // if (body["signature_key"] === trxSignature) {
-    const { data, error } = await supabase.rpc('create_payment', {p_notif_body: JSON.stringify(body)});
-    console.log(data);
-    console.log(error);
-  // }
-
-  res.status(200).send(data);
-});
-
-// Menjalankan server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-module.exports = app;
+export default async function handler(req, res) {
+  const { body } = req;
+  return res.send(`Hello ${body.transaction_time}, you just parsed the request body!`);
+}
