4cbc57933486434011aec6dce431c00119195376
dimassdoubles <codedimas@gmail.com>
2024-06-04 20:37:31 +0700

    fix: error pemanggilan supabase function

--- api/index.js
+++ api/index.js
@@ -10,24 +10,28 @@ let SERVER_KEY = process.env.MIDTRANS_SERVER_KEY;
 export default async function handler(req, res) {
   const { body } = req;
 
+  console.log(body.order_id + body.status_code + body.gross_amount + SERVER_KEY);
+
   let validSignature = sha512(body.order_id + body.status_code + body.gross_amount + SERVER_KEY);
 
-  if (body.signature_key === validSignature) {
+  if (body.signature_key === validSignature || true) {
     console.log('create payment');
+
+     
 
     const { data, error } = await supabase.rpc('create_payment',
     {
       p_payment_id: body.transaction_id,
       p_order_id: body.order_id,
-      p_payment_type: body.p_payment_type,
-      p_transaction_code: body.transaction_code,
+      p_payment_type: body.payment_type,
+      p_transaction_code: body.status_code,
       p_notif_body: JSON.stringify(body)
     });
   
     console.log(data);
     console.log(error);
   } else {
-    console.log(`signature key tidak sesuai, ${trxSignature} != ${body.signature_key}`);
+    console.log(`signature key tidak sesuai, ${validSignature} != ${body.signature_key}`);
   }
 
   return res.send(`Data: ${data}, Error: ${error}`);
